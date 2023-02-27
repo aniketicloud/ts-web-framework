c084b5c89b22557dae83c04d8255ae577bd68b13
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-02-27 21:09:29 +0530

    triggering event callbacks

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,16 @@ import { User } from "./models/User";
 import "./style.css";
 
 const user = new User({ name: "Aniket", age: 30 });
-user.set({ name: "newName" });
-console.log(user.get("name"));
-console.log(user.get("age"));
+user.on("change", () => {
+  console.log("change #1");
+});
+user.on("change", () => {
+  console.log("change #2");
+});
+user.on("save", () => {
+  console.log("save");
+});
+
+user.trigger("save");
+user.trigger("change");
+user.trigger("change");
