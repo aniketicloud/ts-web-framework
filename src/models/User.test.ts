0e7e6bbea18bc66610d95ea0122624f1e6019192
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-02-27 20:58:34 +0530

    removed unnecessary describe

--- src/models/User.test.ts
+++ src/models/User.test.ts
@@ -5,66 +5,64 @@ import { Callback, User, UserInfo } from "./User";
 
 let mockUser: UserInfo;
 let user: User;
-describe("User class", () => {
+beforeEach(() => {
+  mockUser = {
+    name: "user_name",
+    age: 29,
+  };
+  user = new User(mockUser);
+});
+
+describe("get()", () => {
+  it("returns user name & age", () => {
+    expect(user.get("name")).toBe(mockUser.name);
+    expect(user.get("age")).toBe(29);
+  });
+});
+
+describe("set()", () => {
+  let changedUserData: UserInfo;
   beforeEach(() => {
-    mockUser = {
-      name: "user_name",
-      age: 29,
+    changedUserData = {
+      name: "new name",
+      age: 50,
     };
-    user = new User(mockUser);
   });
-
-  describe("get()", () => {
-    it("returns user name & age", () => {
-      expect(user.get("name")).toBe(mockUser.name);
-      expect(user.get("age")).toBe(29);
-    });
+  it("sets user name & age", () => {
+    user.set(changedUserData);
+    expect(user.get("name")).toBe(changedUserData.name);
+    expect(user.get("age")).toBe(changedUserData.age);
   });
-
-  describe("set()", () => {
-    let changedUserData: UserInfo;
-    beforeEach(() => {
-      changedUserData = {
-        name: "new name",
-        age: 50,
-      };
-    });
-    it("sets user name & age", () => {
-      user.set(changedUserData);
-      expect(user.get("name")).toBe(changedUserData.name);
-      expect(user.get("age")).toBe(changedUserData.age);
-    });
-    it("sets only name", () => {
-      user.set({ name: changedUserData.name });
-      expect(user.get("name")).toBe(changedUserData.name);
-      expect(user.get("age")).toBe(mockUser.age);
-    });
-    it("sets only age", () => {
-      user.set({ age: changedUserData.age });
-      expect(user.get("name")).toBe(mockUser.name);
-      expect(user.get("age")).toBe(changedUserData.age);
-    });
+  it("sets only name", () => {
+    user.set({ name: changedUserData.name });
+    expect(user.get("name")).toBe(changedUserData.name);
+    expect(user.get("age")).toBe(mockUser.age);
   });
+  it("sets only age", () => {
+    user.set({ age: changedUserData.age });
+    expect(user.get("name")).toBe(mockUser.name);
+    expect(user.get("age")).toBe(changedUserData.age);
+  });
+});
 
-  describe("on()", () => {
-    const eventOne = "eventOne";
-    const eventTwo = "another-event";
-    const callback: Callback = () =>
-      void it("creates a new event listener with one callback", () => {
-        user.on(eventOne, callback);
-        expect(user.events[eventOne]).toHaveLength(1);
-      });
-    it("adds two callbacks for the same event listener", () => {
+describe("on()", () => {
+  const eventOne = "eventOne";
+  const eventTwo = "another-event";
+  const callback: Callback = () =>
+    void it("creates a new event listener with one callback", () => {
       user.on(eventOne, callback);
-      user.on(eventOne, callback);
-      expect(user.events[eventOne]).toHaveLength(2);
-    });
-    it("adds one callback for eventOne event listener and two callbacks for event listener eventTwo", () => {
-      user.on(eventOne, callback);
-      user.on(eventTwo, callback);
-      user.on(eventTwo, callback);
       expect(user.events[eventOne]).toHaveLength(1);
-      expect(user.events[eventTwo]).toHaveLength(2);
     });
+  it("adds two callbacks for the same event listener", () => {
+    user.on(eventOne, callback);
+    user.on(eventOne, callback);
+    expect(user.events[eventOne]).toHaveLength(2);
+  });
+  it("adds one callback for eventOne event listener and two callbacks for event listener eventTwo", () => {
+    user.on(eventOne, callback);
+    user.on(eventTwo, callback);
+    user.on(eventTwo, callback);
+    expect(user.events[eventOne]).toHaveLength(1);
+    expect(user.events[eventTwo]).toHaveLength(2);
   });
 });
