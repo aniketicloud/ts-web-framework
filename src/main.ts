21e03a7220b5e75930787cf29844816b9bbf5e09
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-03-04 13:48:26 +0530

    checking post request

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,14 @@
-import { User } from "./models/User";
+import { User, UserInfo } from "./models/User";
 import "./style.css";
 
-const user = new User({ name: "Aniket", age: 30 });
-user.on("change", () => {
-  console.log("change #1");
-});
-user.on("change", () => {
-  console.log("change #2");
-});
-user.on("save", () => {
-  console.log("save");
-});
+const baseUrl: string = "http://localhost:3000/users";
+const testUser: UserInfo = { name: "Aniket", age: 30 };
+const user = new User(testUser);
 
-user.trigger("save");
-user.trigger("change");
-user.trigger("change");
+fetch(baseUrl, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(testUser),
+});
