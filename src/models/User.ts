8c8727adf8c82554824d46be0370082b9cabe503
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-03-11 12:51:18 +0530

    Two important rules

--- src/models/User.ts
+++ src/models/User.ts
@@ -11,5 +11,4 @@ export const rootUrl: string = "http://localhost:3000/users";
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserInfo> = new Sync<UserInfo>(rootUrl);
-
 }
