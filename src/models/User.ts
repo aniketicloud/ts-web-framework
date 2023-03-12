4e22ab26dc1a54ae93d3ded6fe06b2b5b390e48e
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-03-12 15:13:07 +0530

    re-Integrating attributes

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { Attributes } from "./Attributes";
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 
@@ -11,4 +12,8 @@ export const rootUrl: string = "http://localhost:3000/users";
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
+  constructor(data: UserProps) {
+    this.attributes = new Attributes<UserProps>(data);
+  }
 }
