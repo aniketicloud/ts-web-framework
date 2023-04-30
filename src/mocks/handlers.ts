ca064ca53df83c3aaa83f86f40fe919705f758b5
Aniket Pandharabale <aniketicloud@gmail.com>
2023-04-30 22:57:15 +0530

    adding test for status.ok = false for fetcher.ts

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -20,4 +20,12 @@ export const handlers = [
   rest.get("http://localhost:3000/users/1", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(users[0]));
   }),
+
+  /**
+   * This handler is for status.ok = false. 
+   * Test file: fetcher.test.ts
+   */
+  rest.get("http://localhost:3000/error-url", (req, res, ctx) => {
+    return res(ctx.status(301));
+  }),
 ];
--- src/utils/fetcher.test.ts
+++ src/utils/fetcher.test.ts
@@ -5,7 +5,7 @@ import { rootUrl, UserProps } from "../models/User";
 import { fetcher } from "./fetcher";
 
 // Start server before all tests
-beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+beforeAll(() => server.listen());
 
 //  Close server after all tests
 afterAll(() => server.close());
@@ -23,5 +23,12 @@ describe("fetcher() with mocking data for localhost url", () => {
   });
   it("throws an error for incorrect url", () => {
     return expect(fetcher("http://some-wrong-url.com")).rejects.toThrowError();
+  });
+  it("thows an error when reponse is not ok", () => {
+    // response.ok = false in case status is not in between 200-299
+    // https://developer.mozilla.org/en-US/docs/Web/API/Response/ok
+    return expect(
+      fetcher("http://localhost:3000/error-url")
+    ).rejects.toThrowError("301: Moved Permanently");
   });
 });
