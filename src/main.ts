ad171ed15064b24b1ce8a42ce1b669c448dc76d1
Aniket Pandharabale <aniketicloud@gmail.com>
2023-10-02 21:26:00 +0530

    model wrapup. Vitest pending

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { User } from "./models/User";
 
-const user1 = new User({ id: 1 });
-console.log("before", user1);
-await user1.fetch();
-console.log("afters", user1);
+const user = User.buildUser({ id: 1 });
+
+user.on("change", () => {
+  console.log(`${user.get("name")} user changed`, user);
+});
+await user.fetch();
